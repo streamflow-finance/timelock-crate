db80c76405a122e636887ad9f2c78d930829be86
Stefan Dragisic <stefan.dragisic@axoniq.io>
2021-11-08 23:48:47 +0100

    Print every custom error

--- src/error.rs
+++ src/error.rs
@@ -1,20 +1,22 @@
 use solana_program::program_error::ProgramError;
 use thiserror::Error;
+use  solana_program::msg;
 
 #[derive(Error, Debug, Copy, Clone)]
 pub enum StreamFlowError {
     #[error("Accounts not writable!")]
-    AccountsNotWritable = 1,
+    AccountsNotWritable,
 
     #[error("Invalid Metadata!")]
-    InvalidMetaData = 2,
+    InvalidMetaData,
 
     #[error("Sender mint does not match accounts mint!")]
-    MintMismatch = 3,
+    MintMismatch,
 }
 
 impl From<StreamFlowError> for ProgramError {
     fn from(e: StreamFlowError) -> Self {
+        msg!(&e.to_string());
         ProgramError::Custom(e as u32)
     }
 }
