956180b63035f83fc1da37714a1f0d3616915b29
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-12-23 17:51:28 +0100

    license: update to Business License (for now)

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,3 @@
-// Copyright (c) 2021 Ivan Jelincic <parazyd@dyne.org>
-//
-// This file is part of streamflow-finance/timelock-crate
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License version 3
-// as published by the Free Software Foundation.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
-
 //! The code providing timelock primitives
 //! used by [streamflow.finance](https://streamflow.finance).
 
