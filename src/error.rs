4f89f47a66529d8f42aea3875a4390e1f833ebea
Nemanja <nemanja.boskovic17@gmail.com>
2022-01-25 14:18:35 +0100

    add message instructions

--- src/error.rs
+++ src/error.rs
@@ -4,46 +4,46 @@ use thiserror::Error;
 #[derive(Error, Debug, Copy, Clone)]
 pub enum SfError {
     #[error("Accounts not writable!")]
-    AccountsNotWritable,
+    AccountsNotWritable = 0x60,
 
     #[error("Invalid Metadata!")]
-    InvalidMetadata,
+    InvalidMetadata = 0x61,
 
     #[error("Invalid metadata account")]
-    InvalidMetadataAccount,
+    InvalidMetadataAccount = 0x62,
 
     #[error("Provided accounts don't match the ones in contract.")]
-    MetadataAccountMismatch,
+    MetadataAccountMismatch = 0x63,
 
     #[error("Invalid escrow account")]
-    InvalidEscrowAccount,
+    InvalidEscrowAccount = 0x64,
 
     #[error("Provided account(s) is/are not valid associated token accounts.")]
-    NotAssociated,
+    NotAssociated = 0x65,
 
     #[error("Sender mint does not match accounts mint!")]
-    MintMismatch,
+    MintMismatch = 0x66,
 
     #[error("Recipient not transferable for account")]
-    TransferNotAllowed,
+    TransferNotAllowed = 0x67,
 
     #[error("Stream closed")]
-    StreamClosed,
+    StreamClosed = 0x68,
 
     #[error("Invalid Streamflow Treasury accounts supplied")]
-    InvalidTreasury,
+    InvalidTreasury = 0x69,
 
     #[error("Given timestamps are invalid")]
-    InvalidTimestamps,
+    InvalidTimestamps = 0x70,
 
     #[error("Deposited amount must be <= Total amount")]
-    InvalidDeposit,
+    InvalidDeposit = 0x71,
 
     #[error("Amount cannot be zero")]
-    AmountIsZero,
+    AmountIsZero = 0x72,
 
     #[error("Amount requested is larger than available")]
-    AmountMoreThanAvailable,
+    AmountMoreThanAvailable = 0x73,
 }
 
 impl From<SfError> for ProgramError {
